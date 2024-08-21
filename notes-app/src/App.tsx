9d3b4c78c33ec60663b6c5fc3a89943e969876ff
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-08-20 22:35:45 -0300

    Adds another sample app: a simple notes app

---
+++ notes-app/src/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./App.css";
+import SideBar from "./components/SideBar/Sidebar";
+
+const DUMMY_NOTES = [
+  {
+    id: "n3",
+    text: "Hey there, Add your note by clicking the plus icon and choosing the theme color",
+    time: "Jul 28, 3:50 PM",
+    color: "#FBA1A1",
+    lock: false,
+  },
+  {
+    id: "n2",
+    text: "You can lock your note by clicking the lock icon and unlock with the same",
+    time: "Jul 25, 8:00 PM",
+    color: "#C4DFAA",
+    lock: false,
+  },
+  {
+    id: "n1",
+    text: "You can delete your note too by clicking the delete icon. \n\nYes..Yes you can delete these instruction notes too",
+    time: "Jul 18, 10:55 AM",
+    color: "#F5F0BB",
+    lock: false,
+  },
+];
+
+const App = () => {
+  const [notes, setNotes] = useState(
+    // JSON.parse(localStorage.getItem("react-notes-app")) || DUMMY_NOTES,
+    DUMMY_NOTES,
+  );
+
+  return (
+    <>
+      <div className="header">
+        <h1>Minimal Notes</h1>
+      </div>
+      <div className="app">
+        <SideBar setNotes={setNotes} />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ notes-app/src/components/SideBar/Sidebar.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import "./SideBar.css";
+import addImage from "../../assets/add.png";
+
+const colorList = [
+  "#C4DFAA",
+  "#F5F0BB",
+  "#D6EFED",
+  "#FBA1A1",
+  "#DCD6F7",
+  "#FFC7C7",
+];
+
+interface SideBarProps {
+  setNotes: (arg: any) => void;
+}
+
+const SideBar = ({ setNotes }: SideBarProps) => {
+  const [isListOpen, setIsListOpen] = useState(false);
+
+  const addNoteHandler = (themeColor: string) => {
+    const note = {
+      id: Date.now() + "" + Math.floor(Math.random() * 78),
+      text: "",
+      time: new Date().toLocaleDateString("en-US", {
+        hourCycle: "h12",
+        minute: "2-digit",
+        day: "2-digit",
+        month: "short",
+        hour: "2-digit",
+      }),
+      color: themeColor,
+      lock: false,
+    };
+
+    setNotes((prevNotes: string[]) => {
+      return [note, ...prevNotes];
+    });
+  };
+
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar__add-img"
+        src={addImage}
+        alt="add image"
+        onClick={() => setIsListOpen(!isListOpen)}
+      />
+
+      <ul
+        className={`sidebar__colorlist ${isListOpen ? "sidebar__colorlist_active" : ""}`}
+      >
+        {isListOpen &&
+          colorList.map((color: string, index: number) => (
+            <li
+              className="sidebar__colorlist_color"
+              key={index}
+              style={{ backgroundColor: color }}
+              onClick={() => addNoteHandler(color)}
+            ></li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SideBar;
